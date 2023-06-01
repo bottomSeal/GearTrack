7fa2cfd8e0cea354588a5f73fd21be33cf5539b9
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-06-01 21:25:55 +0300

    init commit

---
+++ src/main/java/com/example/geartrack/GearTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.geartrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GearTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/geartrack/GearTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geartrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
