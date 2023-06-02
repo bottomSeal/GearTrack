9451a6632c860a8dfffccc5994eb1340631b51d7
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-06-02 11:01:45 +0300

    create user controller messages

---
+++ src/main/java/com/example/geartrack/messages/requests/UserRegisterRequest.java
@@ -0,0 +1,16 @@
+package com.example.geartrack.messages.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegisterRequest {
+
+    private String email;
+
+    private String password;
+
+}
---
+++ src/main/java/com/example/geartrack/messages/response/UserRegisterResponse.java
@@ -0,0 +1,20 @@
+package com.example.geartrack.messages.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegisterResponse {
+
+    private String email;
+
+    private UUID uuid;
+
+    private String token;
+
+}
