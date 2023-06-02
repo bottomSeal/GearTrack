0305fb515a8f9748a12eb9e2f4c767dc44f6dd07
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-06-02 20:33:22 +0300

    add finding bi id and owner

--- src/main/java/com/example/geartrack/repositories/TripRepository.java
+++ src/main/java/com/example/geartrack/repositories/TripRepository.java
@@ -1,9 +1,13 @@
 package com.example.geartrack.repositories;
 
 import com.example.geartrack.entities.TripEntity;
+import com.example.geartrack.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TripRepository extends JpaRepository<TripEntity, UUID> {
+
+    Optional<TripEntity> findByTripIdAndTripOwner(UUID tripId, UserEntity owner) ;
 }
