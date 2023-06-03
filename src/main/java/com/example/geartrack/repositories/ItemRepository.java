e0c449fec45ebd407f66184c6c5c98f8af19e6e3
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-06-03 09:56:27 +0300

    add searching items by hiking type

--- src/main/java/com/example/geartrack/repositories/ItemRepository.java
+++ src/main/java/com/example/geartrack/repositories/ItemRepository.java
@@ -1,9 +1,15 @@
 package com.example.geartrack.repositories;
 
 import com.example.geartrack.entities.ItemEntity;
+import com.example.geartrack.models.enums.HikingType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ItemRepository extends JpaRepository<ItemEntity, UUID> {
+
+    @Query("SELECT i FROM ItemEntity i WHERE i.hikingType = 'ALL' OR i.hikingType = :hikingType")
+    List<ItemEntity> findByHikingType(HikingType hikingType);
 }
