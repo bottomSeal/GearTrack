7abfde70406d9eeae4430b66cccaa25317414160
Egor Petrenko <petrenkoegor.2110@gmail.com>
2023-06-04 18:07:55 +0300

    rewrite query

--- src/main/java/com/example/geartrack/repositories/ItemRepository.java
+++ src/main/java/com/example/geartrack/repositories/ItemRepository.java
@@ -4,12 +4,17 @@ import com.example.geartrack.entities.ItemEntity;
 import com.example.geartrack.models.enums.HikingType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface ItemRepository extends JpaRepository<ItemEntity, UUID> {
 
-    @Query("SELECT i FROM ItemEntity i WHERE i.hikingType = 'ALL' OR i.hikingType = :hikingType")
-    List<ItemEntity> findByHikingType(HikingType hikingType);
+    @Query("""
+    SELECT i 
+    FROM ItemEntity i 
+    WHERE i.hikingType = 'ALL' OR i.hikingType = :hikingType
+    """)
+    List<ItemEntity> findByHikingType(@Param("hikingType")HikingType hikingType);
 }
